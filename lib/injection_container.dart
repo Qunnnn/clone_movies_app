b7ee326bc2069cebd4674a80504c971128b9fd5f
Qunnnn <vantranquan123@gmail.com>
2023-08-01 15:03:21 +0700

    Fix

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -9,87 +9,66 @@ import 'package:get_it/get_it.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //Intro
-  sl.registerFactory<UpComingMovieBloc>(
-      () => UpComingMovieBloc(getUpComingMovie: sl()));
+  sl
+    //Intro
+    ..registerFactory<UpComingMovieBloc>(
+        () => UpComingMovieBloc(getUpComingMovie: sl()))
+    ..registerLazySingleton<GetUpComingMovie>(
+        () => GetUpComingMovie(repository: sl()))
+    ..registerLazySingleton<UpComingRepository>(
+        () => UpComingMovieRepositoryIml(remoteDataSource: sl()))
+    ..registerLazySingleton<RemoteDataSource>(
+        () => RemoteDataSourceIml(netWorkManager: sl()))
+    ..registerLazySingleton<NetworkManager>(() => NetWorkManagerIml());
 
-  sl.registerLazySingleton<GetUpComingMovie>(
-      () => GetUpComingMovie(repository: sl()));
+  sl
+    //Main
+    ..registerFactory<MainPageBloc>(() => MainPageBloc());
 
-  sl.registerLazySingleton<UpComingRepository>(
-      () => UpComingMovieRepositoryIml(remoteDataSource: sl()));
+  sl
+    //Home
+    ..registerFactory<PopularTvBloc>(() => PopularTvBloc(getPopularTv: sl()))
+    ..registerLazySingleton<GetPopularTv>(
+        () => GetPopularTv(popularTvRepo: sl()))
+    ..registerLazySingleton<PopularTvRepo>(
+        () => PopularTvRepoIml(popularTvRemoteDataSoure: sl()))
+    ..registerLazySingleton<PopularTvRemoteDataSoure>(
+        () => PopularTvRemoteDataSoureIml(networkManager: sl()))
+    ..registerFactory<TopRatedMovieBloc>(
+        () => TopRatedMovieBloc(getTopRatedMovie: sl()))
+    ..registerLazySingleton<GetTopRatedMovie>(
+        () => GetTopRatedMovie(topRatedMovieRepository: sl()))
+    ..registerLazySingleton<TopRatedMovieRepository>(
+        () => TopRatedRepositoryIml(topRatedMovieRemoteDataSoure: sl()))
+    ..registerLazySingleton<TopRatedMovieRemoteDataSoure>(
+        () => TopRatedMovieRemoteDataSoureIml(networkManager: sl()))
+    ..registerFactory<TrendingMovieBloc>(
+        () => TrendingMovieBloc(getTrendingMovie: sl()))
+    ..registerLazySingleton<GetTrendingMovie>(
+        () => GetTrendingMovie(trendingMovieRepository: sl()))
+    ..registerLazySingleton<TrendingMovieRepository>(
+        () => TrendingMovieRepositoryIml(trendingMovieRemoteDataSoure: sl()))
+    ..registerLazySingleton<TrendingMovieRemoteDataSoure>(
+        () => TrendingMovieRemoteDataSoureIml(networkManager: sl()));
 
-  sl.registerLazySingleton<RemoteDataSource>(
-      () => RemoteDataSourceIml(netWorkManager: sl()));
-
-  sl.registerLazySingleton<NetworkManager>(() => NetWorkManagerIml());
-
-  //Main
-  sl.registerFactory<MainPageBloc>(() => MainPageBloc());
-
-  //Home
-  sl.registerFactory<PopularTvBloc>(() => PopularTvBloc(getPopularTv: sl()));
-
-  sl.registerLazySingleton<GetPopularTv>(
-      () => GetPopularTv(popularTvRepo: sl()));
-
-  sl.registerLazySingleton<PopularTvRepo>(
-      () => PopularTvRepoIml(popularTvRemoteDataSoure: sl()));
-
-  sl.registerLazySingleton<PopularTvRemoteDataSoure>(
-      () => PopularTvRemoteDataSoureIml(networkManager: sl()));
-
-  sl.registerFactory<TopRatedMovieBloc>(
-      () => TopRatedMovieBloc(getTopRatedMovie: sl()));
-
-  sl.registerLazySingleton<GetTopRatedMovie>(
-      () => GetTopRatedMovie(topRatedMovieRepository: sl()));
-
-  sl.registerLazySingleton<TopRatedMovieRepository>(
-      () => TopRatedRepositoryIml(topRatedMovieRemoteDataSoure: sl()));
-
-  sl.registerLazySingleton<TopRatedMovieRemoteDataSoure>(
-      () => TopRatedMovieRemoteDataSoureIml(networkManager: sl()));
-
-  sl.registerFactory<TrendingMovieBloc>(
-      () => TrendingMovieBloc(getTrendingMovie: sl()));
-
-  sl.registerLazySingleton<GetTrendingMovie>(
-      () => GetTrendingMovie(trendingMovieRepository: sl()));
-
-  sl.registerLazySingleton<TrendingMovieRepository>(
-      () => TrendingMovieRepositoryIml(trendingMovieRemoteDataSoure: sl()));
-
-  sl.registerLazySingleton<TrendingMovieRemoteDataSoure>(
-      () => TrendingMovieRemoteDataSoureIml(networkManager: sl()));
-
-  //Detail
-
-  sl.registerFactory<DetailsBloc>(() => DetailsBloc(getDetails: sl()));
-
-  sl.registerLazySingleton<GetDetails>(() => GetDetails(detailsRepo: sl()));
-
-  sl.registerLazySingleton<DetailsRepo>(
-      () => DetailsRepoIml(detailsRemoteDataSource: sl()));
-
-  sl.registerLazySingleton<DetailsRemoteDataSource>(
-      () => DetailsRemoteDataSourceIml(networkManager: sl()));
-
-  sl.registerFactory<CastBloc>(() => CastBloc(getCasts: sl()));
-
-  sl.registerLazySingleton<GetCasts>(() => GetCasts(castRepo: sl()));
-
-  sl.registerLazySingleton<CastRepo>(
-      () => CastRepoIml(castRemoteDataSource: sl()));
-
-  sl.registerLazySingleton<CastRemoteDataSource>(
-      () => CastRemoteDataSourceIml(networkManager: sl()));
-
-  sl.registerFactory<TrailerBloc>(() => TrailerBloc(getTrailer: sl()));
-
-  sl.registerLazySingleton<GetTrailer>(() => GetTrailer(trailerRepo: sl()));
-
-  sl.registerLazySingleton<TrailerRepo>(() => TrailerRepoIml(trailerRemoteDataSource: sl()));
-
-  sl.registerLazySingleton<TrailerRemoteDataSource>(() => TrailerRemoteDataSourceIml(networkManager: sl()));
+  sl
+    //Detail
+    ..registerFactory<DetailsBloc>(() => DetailsBloc(getDetails: sl()))
+    ..registerLazySingleton<GetDetails>(() => GetDetails(detailsRepo: sl()))
+    ..registerLazySingleton<DetailsRepo>(
+        () => DetailsRepoIml(detailsRemoteDataSource: sl()))
+    ..registerLazySingleton<DetailsRemoteDataSource>(
+        () => DetailsRemoteDataSourceIml(networkManager: sl()))
+    ..registerFactory<CastBloc>(() => CastBloc(getCasts: sl()))
+    ..registerLazySingleton<GetCasts>(() => GetCasts(castRepo: sl()))
+    ..registerLazySingleton<CastRepo>(
+        () => CastRepoIml(castRemoteDataSource: sl()))
+    ..registerLazySingleton<CastRemoteDataSource>(
+        () => CastRemoteDataSourceIml(networkManager: sl()))
+    ..registerFactory<TrailerBloc>(() => TrailerBloc(getTrailer: sl()))
+    ..registerLazySingleton<GetTrailer>(() => GetTrailer(trailerRepo: sl()))
+    ..registerLazySingleton<TrailerRepo>(
+        () => TrailerRepoIml(trailerRemoteDataSource: sl()))
+    ..registerLazySingleton<TrailerRemoteDataSource>(
+        () => TrailerRemoteDataSourceIml(networkManager: sl()));
 }
